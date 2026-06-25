c8134926d06fe5fbc685f9ecf14959531c8b1928
mohd amin <sheikmohdamin@gmail.com>
2026-06-25 16:53:51 +0530

    updated code

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Hello World "*3)
+# print("Hello World","world",sep="#")
+# print("Hello World",end=" ")
+# print("Hello World")
+# print("a">"b")
+# x = "10.5"
+# print(int(float(x)))
+# print("\test\ntesting")
+# print(f"\test\ntesting")
+# print("\\test\\ntesting")
+
+# print(""" hello
+#         my name
+#         is john""")
+# x = "hello"
+# print(x[-1])
+# print(x[2])
+# for i in range(0,10,2):
+#     print(i)
+while True:
+    print("Hello")
